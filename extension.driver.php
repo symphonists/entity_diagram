ba8457c01fb732bfba7f6af68ff17346578661a9
John Porter <john.porter@designermonkey.co.uk>
2010-09-15 10:50:05 +0100

    Added logic to prevent error on no sections. Also disables button on no sections

--- extension.driver.php
+++ extension.driver.php
@@ -1,8 +1,8 @@
 <?php
 	require_once(TOOLKIT . '/class.sectionmanager.php');
-	
+
 	Class extension_entity_diagram extends Extension {
-	
+
 		public function about(){
 			return array(
 				'name'			=> 'Entity Diagram',
@@ -23,7 +23,7 @@
 				)
 			);
 		}
-		
+
 		public function getSubscribedDelegates(){
 			return array(
 				array(
@@ -33,9 +33,9 @@
 				),
 			);
 		}
-		
+
 		public function export($section_ids){
-			
+
 			$output = 'digraph g {
 
 			graph [
@@ -47,18 +47,18 @@
 				shape = plaintext
 				fontname = Arial
 			];';
-			
+
 			$sm = new SectionManager(Administration::instance());
 		  	$sections = $sm->fetch();
-		
+
 			$relationships = array();
-			
+
 			// get all section links
 			$section_associations = $this->_Parent->Database->fetch("SELECT * FROM tbl_sections_association");
-			
+
 			foreach($sections as $section) {
 				if (!in_array($section->get('id'), $section_ids)) continue;
-				
+
 				$output .= sprintf(
 					's%d [
 						label = <<table border="0" cellborder="1" cellspacing="0" color="#666666"><tr><td bgcolor="#dddddd" port="h%d"><font color="#000000">%s</font></td></tr>',
@@ -66,11 +66,11 @@
 					$section->get("id"),
 					$section->get("name")
 				);
-				
+
 				// get list of fields
 				$fields = $section->fetchFields();
 				if (!is_array($fields)) $fields = array();
-				
+
 				foreach($fields as $field) {
 
 					$output .= sprintf('<tr><td port="f%d" align="left"><font color="#333333">%s</font> <font color="#aaaaaa" point-size="10">%s</font></td></tr>',
@@ -78,7 +78,7 @@
 						$field->get("label"),
 						$field->get('type')
 					);
-					
+
 					// find a section link involving this field
 					foreach ($section_associations as $relationship) {
 
@@ -92,7 +92,7 @@
 							// get the child section and the child field
 							$child_section = $sm->fetch($relationship["child_section_id"]);
 							$child_section_field = $section->_fieldManager->fetch($relationship["child_section_field_id"]);
-							
+
 							// if a parent
 							if ($field->get("id") == $relationship["parent_section_field_id"]) {
 								if ($child_section_field) {
@@ -102,7 +102,7 @@
 									$linked_section_handle = $child_section->_data["handle"];
 									$linked_field_name = $child_section_field->get("label");
 									$linked_field_id = $child_section_field->get("id");
-								}								
+								}
 							} else {
 								if ($parent_section_field) {
 									// get the properties of this relationship
@@ -113,28 +113,28 @@
 									$linked_field_id = $parent_section_field->get("id");
 								}
 							}
-							
+
 							if ($linked_section_id && in_array($linked_section_id, $section_ids)) {
 								$relationships[] = array($section->get("id"), $field->get("id"), $linked_section_id, $linked_field_id);
 							}
-							
+
 						}
-						
+
 					}
-					
+
 					if ($field->get('type') == 'subsectionmanager') {
 						$child_section = $sm->fetch($field->get('subsection_id'));
 						$relationships[] = array($section->get("id"), $field->get("id"), $child_section->get('id'), null);
 					}
-					
+
 				}
-				
+
 				$output .= sprintf('
 						</table>>];'
 				);
 
 			}
-			
+
 			foreach($relationships as $rel) {
 				$output .= sprintf(
 					's%d:f%d -> %s [arrowsize = 0.5, color="#666666"];',
@@ -143,26 +143,26 @@
 					(($rel[3] == null) ? 's' . $rel[2] . ':h' . $rel[2] : 's' . $rel[2] . ':f' . $rel[3])
 				);
 			}
-			
+
 			$output .= '}';
-			
+
 			$file_path = MANIFEST . '/tmp/';
 			$file_name = Lang::createHandle(Administration::instance()->Configuration->get('sitename', 'general')) . '-' . date('Ymd', time()) . '.gv';
-			
+
 			file_put_contents($file_path . $file_name, $output);
-			
+
 			header('Content-type: application/.gv');
 			header('Expires: ' . gmdate('D, d M Y H:i:s') . ' GMT');
 		    header('Content-disposition: attachment; filename=' . $file_name);
 		    header('Pragma: no-cache');
-		
+
 			readfile($file_path . $file_name);
 			exit();
-			
+
 		}
-		
+
 		public function appendPreferences($context){
-			
+
 			if(isset($_POST['action']['entity-diagram-graphviz-export'])){
 				$this->export($_POST['fields']['entity-diagram-sections']);
 			}
@@ -173,15 +173,22 @@
 
 			$aTableBody = array();
 			$options = array();
-			
+
 			$sm = new SectionManager($this->_Parent);
 		  	$sections = $sm->fetch(NULL, 'ASC', 'name');
-			
+
 			$selected = true;
-			foreach($sections as $section) {
-				$options[] = array($section->get('id'), $selected, $section->get('name'));
+
+			if(!empty($sections)){
+				foreach($sections as $section) {
+					$options[] = array($section->get('id'), $selected, $section->get('name'));
+					$attributes = NULL;
+				}
+			}else{
+				$options = NULL;
+				$attributes = array('disabled'=>'disabled');
 			}
-			
+
 			$label = Widget::Label(
 				'Included Sections',
 				Widget::Select(
@@ -196,13 +203,13 @@
 
 			$group->appendChild($label);
 			$group->appendChild(
-				Widget::Input('action[entity-diagram-graphviz-export]', 'Export Graphviz', 'submit')
+				Widget::Input('action[entity-diagram-graphviz-export]', 'Export Graphviz', 'submit', $attributes)
 			);
-							
+
 			$context['wrapper']->appendChild($group);
-					
+
 		}
-			
+
 	}
 
 ?>
